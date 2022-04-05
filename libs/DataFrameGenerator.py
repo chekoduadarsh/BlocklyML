19384db9b8e3ab963e02fa8640cd956563deeeb2
chekoduadarsh <chekodu.adarsh@gmail.com>
2022-04-05 18:43:08 +0530

    Code clean and optimization

--- libs/DataFrameGenerator.py
+++ libs/DataFrameGenerator.py
@@ -9,18 +9,17 @@ pd.set_option('display.max_columns', None)
 pd.set_option('display.width', None)
 pd.set_option('display.max_colwidth', -1)
 
-def DataFramevisualizer(codeImdict,dash_app):
+
+def DataFramevisualizer(codeImdict, dash_app):
     codedict = dict(codeImdict)
     listDF = []
-    
+
     for code in codedict.keys():
         loc = {}
         replaceStr = ""
-        command = "var = "+code.replace('"',"'")+replaceStr
+        command = "var = "+code.replace('"', "'")+replaceStr
         exec(command, globals(), loc)
-        dash_app = dashboardApp(loc['var'],dash_app,{})
+        dash_app = dashboardApp(loc['var'], dash_app, {})
         listDF.append("")
 
     return listDF, dash_app
-
-
